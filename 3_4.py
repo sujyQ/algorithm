697154905350dec42e43b605912a66d914a2ab7d
sujyQ <tn0502wl@ajou.ac.kr>
2021-07-15 23:27:10 +0900

    add efficient code

--- 3_4.py
+++ 3_4.py
@@ -12,3 +12,26 @@ while N != 1 :
     cnt = cnt + 1
 
 print(cnt)
+
+# second one
+N, K = map(int, sys.stdin.readline().split())
+
+cnt = 0
+while True :
+    target = (N//K)*K       # target은 N 이하의 수 중 최대인 K의 배수
+    cnt += (N-target)       # cnt은 N과 target의 차이 : 1씩 빼기
+    N = target              # N은 K로 나누어 떨어지는 수가 됨
+
+    if N < K :
+        break
+
+    cnt += 1                # K로 나누기
+    N //= K
+
+cnt += (N-1)                # N이 K보다 작은 값일 때 1씩 빼기
+print(cnt)
+
+# test case : N = 25, K = 3
+# 1. target = 24, cnt = 2, N = 8
+# 2. target = 6, cnt = 5, N = 2
+# 3. break -> cnt = 6
