a7d8f967d977632c8f4974b328a4e75d76bb62eb
sujyQ <tn0502wl@ajou.ac.kr>
2021-07-24 22:42:45 +0900

    add answer

--- 4_1.py
+++ 4_1.py
@@ -1,5 +1,7 @@
 import sys
 
+###### my answer
+
 N = int(sys.stdin.readline())
 plan = sys.stdin.readline().split()
 
@@ -23,3 +25,26 @@ for i in plan :
 
 print(pos[1], pos[0])
 
+
+##### answer example
+
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+# moving directions according to L, R, U, D
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moving_types = ['L', 'R', 'U', 'D']
+
+# check moving plans one by one
+for plan in plans :
+    for i in range(len(moving_types)) :
+        if plan == moving_types[i] :
+            nx = x + dx[i]
+            ny = y + dy[i]
+        if nx < 1 or ny < 1 or nx > n or ny > n :
+            continue
+        x, y = nx, ny
+
+print(x, y)
